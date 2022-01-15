c402dfa068f9d967cbb3737f6513edf140e5ece8
Patrick <wpmjones@gmail.com>
2022-01-14 18:05:11 -0800

    testing links endpoint

--- app.py
+++ app.py
@@ -18,15 +18,17 @@ async def root():
     return {"message": "Hello world"}
 
 
-# @app.get("/links")
-# async def get_links(tag_or_id: str):
-#     try:
-#         # Try and convert input to int
-#         # If successful, it's a Discord ID
-#         discordId = int(tag_or_id)
-#         playerTag = ""
-#     except ValueError:
-#         # If it fails, it's a player tag
-#         playerTag = tag_or_id
-#         discordId = 0
-#         logger.info(playerTag)
+@app.get("/links/{tag_or_id}")
+async def get_links(tag_or_id: str):
+    try:
+        # Try and convert input to int
+        # If successful, it's a Discord ID
+        discordId = int(tag_or_id)
+        playerTag = "blank"
+    except ValueError:
+        # If it fails, it's a player tag
+        playerTag = tag_or_id
+        discordId = 0
+        logger.info(playerTag)
+    response = Link(playerTag=playerTag, discordId=discordId)
+    return response
