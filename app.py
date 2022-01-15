625d67ba8f72d777f1b4eae6c75be4a6f3a751ad
Patrick <wpmjones@gmail.com>
2022-01-14 22:49:34 -0800

    testing batch

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ class Link(BaseModel):
 
 @app.get("/links/{tag_or_id}")
 async def get_links(tag_or_id: str, response: Response):
+    if tag_or_id.lower() == "batch":
+        resp = await get_batch(tag_or_id)
+        return resp
     conn = await asyncpg.connect(dsn=creds.pg)
     tags = []
     try:
@@ -44,8 +47,7 @@ async def get_links(tag_or_id: str, response: Response):
     return tags
 
 
-@app.get("/links/batch")
-async def get_batch(user_input: list, response: Response):
+async def get_batch(user_input):
     logger.info(user_input)
     conn = await asyncpg.connect(dsn=creds.pg)
     tags = []
@@ -65,9 +67,6 @@ async def get_batch(user_input: list, response: Response):
                 player_tag = item.upper()
             else:
                 player_tag = f"#{item.upper()}"
-            if not tag_validator.match(player_tag):
-                response.status_code = status.HTTP_400_BAD_REQUEST
-                return "Not a valid player tag."
             sql = "SELECT discordid FROM coc_discord_links WHERE playertag = $1"
             discord_id = await conn.fetchval(sql, player_tag)
             tags.append({"playerTag": player_tag, "discordId": discord_id})
